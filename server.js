32b91ac4e854d5e5e5c7de72261075a1ada53c33
ejero <rositaemakpo@gmail.com>
2023-07-25 21:17:51 -0700

    Fix bug were api calls were blocked by cors

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const { sequelize } = require("./database/db");
 const path = require("path");
 const cors = require("cors");
 
+// Use CORS middleware to allow requests from all origins (*)
+app.use(cors());
+
+// Defining Routers
 const router = require(path.join(__dirname, "routes", "accounts"));
 const routerUser = require(path.join(__dirname, "routes", "user"));
 
@@ -11,6 +15,10 @@ const routerUser = require(path.join(__dirname, "routes", "user"));
 // Testing comment
 app.use("/", router);
 app.use("/user", routerUser);
-// Use CORS middleware to allow requests from all origins (*)
-app.use(cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 module.exports = { app, sequelize };
