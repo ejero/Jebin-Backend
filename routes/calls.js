796104cc597dbf4e35b75822df6804fc000f97c1
ejero <rositaemakpo@gmail.com>
2023-07-27 06:24:46 -0700

    Add routes to delete a call info

--- routes/calls.js
+++ routes/calls.js
@@ -30,4 +30,40 @@ callRouter.post("/create-call", async (req, res) => {
   }
 });
 
+// Route to delete a call by its ID
+callRouter.delete("/delete-call/:id", async (req, res) => {
+  try {
+    const callId = req.params.id;
+
+    // Find the call entry by ID
+    const callData = await CallData.findByPk(callId);
+
+    if (!callData) {
+      return res.status(404).json({ error: "Call data not found." });
+    }
+
+    // Delete the call entry
+    await callData.destroy();
+
+    // Respond with success message
+    res.json({ message: "Call data deleted yay!." });
+  } catch (error) {
+    console.error("Error deleting CallData:", error);
+    res.status(500).json({ error: "Failed to delete call data :(" });
+  }
+});
+
+// Route to get all call data
+callRouter.get("/get-all-calls", async (req, res) => {
+  try {
+    // Retrieve all call data from the database
+    const allCallData = await CallData.findAll();
+
+    // Return all calls
+    res.json(allCallData);
+  } catch (error) {
+    console.error("Error fetching CallData:", error);
+    res.status(500).json({ error: "Failed to fetch call data :(" });
+  }
+});
 module.exports = callRouter;
